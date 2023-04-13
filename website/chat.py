50e00253b9e7c31f17c0938448407d012f4dc1db
Nikhil Baghel <nickevil99@gmail.com>
2023-04-13 14:45:28 +0530

    chore

--- website/chat.py
+++ website/chat.py
@@ -2,7 +2,7 @@
 # from langchain.agents import load_tools
 # from langchain.agents import initialize_agent
 # from langchain.llms import OpenAI
-# os.environ["OPENAI_API_KEY"] = "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" 
+# os.environ["OPENAI_API_KEY"] = "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" 
 # os.environ["SERPAPI_API_KEY"] = "607f32e584a1aec491b0e01fbaa52bf82b6a647603c85c6a657b449ff56f6af0"
 
 # llm = OpenAI(temperature=0)
@@ -17,7 +17,7 @@ from langchain.document_loaders import DirectoryLoader
 import magic
 import os
 import nltk 
-os.environ["OPENAI_API_KEY"] = "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"
+os.environ["OPENAI_API_KEY"] = "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"
 loader = DirectoryLoader('C:\\Users\\nikhi\\Downloads\\newfolder4',glob='**/*.csv')
 docs = loader.load()
 char_text_splitter = CharacterTextSplitter(chunk_size=1000, chunk_overlap=0)
